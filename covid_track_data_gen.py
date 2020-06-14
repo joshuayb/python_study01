8f4a890c865f7c12341d5530a73a3c343edd6db4
joshuayb <joshuayb@gmail.com>
2020-06-13 19:33:10 -0700

    Update covid_track_data_gen.py

--- covid_track_data_gen.py
+++ covid_track_data_gen.py
@@ -1,24 +1,27 @@
-'''from random import *
-a = 0
-for i in range(1,51):
-    place = randint(1,10)
-    if 9<= place <=10:
-        print ("{0}H{1}" . format(i, place))
-        a += 1
-    else:
-        print ("{0}W{1}" . format(i, place))
-print ("total at home: {0}". format(a))'''
-
+import csv  
 from random import *
-a = 0
-for i in range(1,51):
-    place = randint(1,10)
-    if 9 <= place <= 10:
-        geo = str(i) + 'H' + str(place)
-        a += 1
-        print (geo)
-    else:
-        geo = str(i) + 'W' + str(place)
-        print (geo)
-        
-print ("total at home: {0}". format(a))
+
+ref = [['P' + str(i), 'H' + str(randint(1,10)), 'W' + str(round(abs(10 * normalvariate(0,1))))] for i in range(1,20)]
+
+fields = ['id', 'geo']  
+rows = [['P' + str(i+1), ref[i][1] if random() > 0.2 else ref[i][2] if random() > 0.2 else 'S' + str(round(abs(20 * normalvariate(0,1))))] for i in range(0,19)]
+print (rows)
+
+rows2 = [['P' + str(i+1), rows[i][1] if random() > 0.4 else ref[i][1] if random() > 0.2 else ref[i][2] if random() > 0.2 else 'S' + str(round(abs(20 * normalvariate(0,1))))] for i in range(0,19)]
+print (rows2)
+
+filename = "mycsv1.csv" 
+# writing to csv file  
+with open(filename, 'w') as csvfile:  
+    # creating a csv writer object  
+    csvwriter = csv.writer(csvfile)  
+    csvwriter.writerow(fields)   
+    csvwriter.writerows(rows) 
+
+filename = "mycsv2.csv" 
+# writing to csv file  
+with open(filename, 'w') as csvfile:  
+    # creating a csv writer object  
+    csvwriter = csv.writer(csvfile)  
+    csvwriter.writerow(fields)   
+    csvwriter.writerows(rows2) 
